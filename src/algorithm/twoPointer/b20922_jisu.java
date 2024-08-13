8b08a5d049c86e00673be36b1b5ed95d0a145fd8
serot <serotonins023@gmail.com>
2024-08-13 15:00:35 +0900

    algo: jisu_b20922

---
+++ src/algorithm/twoPointer/b20922_jisu.java
@@ -0,0 +1,46 @@
+package algorithm.twoPointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b20922_jisu {
+
+    static int n, inf = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] count = new int[100001];
+        count[nums[0]]++;
+        int answer = 1;
+        int start = 0, end = 0;
+        while (start <= end && end < n) {
+            if (count[nums[end]] > k) {
+                count[nums[start]]--;
+                start++;
+            } else {
+                answer = Math.max(end-start+1, answer);
+                end++;
+                if (end < n) {
+                    count[nums[end]]++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
